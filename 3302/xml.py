769047edd51efc57127548da232dc20b9ce953c4
Cyrill Mendoza <mendozacyrill@gmail.com>
2022-07-25 14:01:57 +0800

    My first commit
    Added 5 files that are used as an example in class

---
+++ 3302/xml.py
@@ -0,0 +1,22 @@
+def convert(data):
+    xml = '<languages>'
+    for i in data:
+        lang = """
+        <language level="{}">
+            <name>{}</name>
+            <creator>{}</creator>
+        </language>
+        """.format(i[2], i[0], i[1])
+        xml += lang
+    xml += '\n</languages>'
+    return xml
+
+languages = [
+    ["C++", "Bjarne Stroustrup", "Low"],
+    ["C", "Dennis Ritchie", "Low"],
+    ["Java", "James Gosling", "High"],
+    ["Python", "Guido van Rossum", "High"]
+]
+
+converted = convert(languages)
+print(converted)
